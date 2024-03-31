b44e0380dea5158792f43afd45e351679c8273cc
Skomorac <admir.skomorac@outlook.com>
2024-03-31 14:31:23 +0000

    added analytics

--- src/js/layout.js
+++ src/js/layout.js
@@ -11,6 +11,8 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
+import { Analytics } from "@vercel/analytics/react"
+
 //create your first component
 const Layout = () => {
   //the basename is used when your project is published in a subdirectory and not in the root of the domain
@@ -32,6 +34,7 @@ const Layout = () => {
           <Footer />
         </ScrollToTop>
       </BrowserRouter>
+      <Analytics />
     </div>
   );
 };
